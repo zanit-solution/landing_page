d6d304cd0b275bfd46688dd247aa8b2cedd91629
getch-hasan <hasan15-3112@diu.edu.bd>
2024-07-13 16:06:47 +0600

    feat:technology details

--- src/app/technologies/[id]/page.js
+++ src/app/technologies/[id]/page.js
@@ -1,24 +1,26 @@
-
 "use client"
-import { useResetProjection } from 'framer-motion';
+import React from 'react';
 import Image from 'next/image';
 import { useParams, usePathname, useRouter, useSearchParams } from 'next/navigation';
-import React from 'react';
 
 const TechnologyDetail = () => {
 
     const router = useSearchParams()
-    console.log(router.get("search"))
-    const data = JSON.parse(router.get("search"))
-    console.log(data?.image)
+    console.log(router)
+    const searchParam = router.get("search");
+    console.log(searchParam, '------------------>')
+    const data = JSON.parse(searchParam)
+    console.log(data)
+    const colorSeq = ['bg-[#61DBFB]', 'bg-[#68A063]', 'bg-[#ff0000]', 'bg-[#3fa037]', 'bg-[#303030]', 'bg-[#007ACC]']
     return (
-        <div className={data?.color}>
-            <div className='container mx-auto py-32 flex flex-row justify-center gap-10 items-center'>
+        <div className={`${colorSeq[parseInt(data?.id)
+            - 1]}`}>
+            <div className='container mx-auto py-10 grid grid-cols-2 justify-around gap-10 items-center'>
                 <div>
-                    <h1 className='text-5xl font-bold'>{data.name}</h1>
-                    <p className='text-xl py-10'>{data.description}</p>
+                    <h1 className='text-5xl font-bold'>{data?.name}</h1>
+                    <p className='text-xl py-10'>{data?.description}</p>
                 </div>
-                <div><Image height={1000} width={1000} src={data?.image} alt="" /></div>
+                <div><Image className='w-sm h-sm' height={1000} width={1000} src={data?.image} alt="" /></div>
 
             </div>
         </div>
